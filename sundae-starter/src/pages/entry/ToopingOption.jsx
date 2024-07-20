8d8ab929011e23ac3bd527c86410e61a9c07159d
Diogo Silva <imdiogosilva@gmail.com>
2024-07-20 14:58:09 +0100

    test(Toppings): add topping tests

--- sundae-starter/src/pages/entry/ToopingOption.jsx
+++ sundae-starter/src/pages/entry/ToopingOption.jsx
@@ -1,9 +1,32 @@
-import { Col } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import { useOrderDetails } from "../../contexts/OrderDetails.jsx";
+import Form from "react-bootstrap/Form";
 
 export default function ToppingOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img src={`http://localhost:3030/${imagePath}`} alt={`${name} topping`} />
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: "10px" }}
+      >
+        <Form.Label column xs="6" style={{ textAlign: "right" }}>
+          {name}
+        </Form.Label>
+
+        <Col xs="5" style={{ textAlign: "left" }}>
+          <Form.Control
+            type="number"
+            defaultValue={0}
+            onChange={(e) =>
+              updateItemCount(name, parseInt(e.target.value), "toppings")
+            }
+          />
+        </Col>
+      </Form.Group>
     </Col>
   );
 }
--- sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -27,3 +27,38 @@ test("update scoops subtotal when scoops change", async () => {
   await user.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update toppings subtotal when toppings change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  //Make sure total starts out at $0.00
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  // update cherries toppings to 1 and check subtotal
+  const cherriesInput = await screen.findByRole("spinbutton", {
+    name: "Cherries",
+  });
+  await user.clear(cherriesInput);
+  await user.type(cherriesInput, "1");
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  //update M&Ms toppings to 2 and check subtotal
+  const mmsTopping = await screen.findByRole("spinbutton", {
+    name: "M&Ms",
+  });
+  await user.clear(mmsTopping);
+  await user.type(mmsTopping, "2");
+  expect(toppingsSubtotal).toHaveTextContent("4.50");
+
+  //update hot fudge scoops to 3 and check subtotal
+  const hotFudgeTopping = await screen.findByRole("spinbutton", {
+    name: "Hot fudge",
+  });
+  await user.clear(hotFudgeTopping);
+  await user.type(hotFudgeTopping, "3");
+  expect(toppingsSubtotal).toHaveTextContent("9.00");
+});
